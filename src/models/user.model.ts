f12ed57bff921461aa1b144d11c1c70c2f55bb2f
Matheus Caldas <matheuscaldasrj@gmail.com>
2017-06-10 01:25:44 -0300

    Adding basic user model class

---
+++ src/models/user.model.ts
@@ -0,0 +1,15 @@
+export class User {
+    
+    name: string;
+    email: string;
+    photo: string;
+
+
+    constructor(name? : string, email? : string, photo? : string){
+        this.name = name;
+        this.email = email;
+        this.photo = photo;
+      
+    }
+
+}
