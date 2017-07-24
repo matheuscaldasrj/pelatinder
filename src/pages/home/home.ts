7e62b54f1c70f17a40cf3588b88c8bfe6a68f374
Matheus Caldas <matheuscaldasrj@gmail.com>
2017-07-23 21:09:08 -0300

    removing useless logs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -21,10 +21,7 @@ export class HomePage {
   constructor(public navCtrl: NavController,public navParams: NavParams, public firebaseService: FirebaseService, private afAuth: AngularFireAuth) {
       console.log("Construtor do home")
       const authObserver = afAuth.authState.subscribe(user=>{
-        console.log("1111111111111111")
         if(user){
-          console.log("222222222222222")
-          console.log(user);
           this.user = new User();
           this.user.name = user.displayName;
           this.user.photo = user.photoURL;
